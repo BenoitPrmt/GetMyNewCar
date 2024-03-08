86a72ca229e5cb952b8343ab8fc4c57c42d27026
KilianOlry <kilian.olry@gmail.com>
2024-03-08 12:15:45 +0100

    fixe(tailwind): add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/views/*.{phtml,js}"],
+  content: ["./src/views/*.{phtml,js}",
+"./src/views/global/*.{phtml,js}"],
   darkMode: 'class',
   theme: {
     extend: {
