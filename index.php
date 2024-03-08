0a218365e5d7bc95c73aec131d35e4c3ec6ae069
BenoitPrmt <benoitparmentier45@gmail.com>
2024-03-08 09:45:08 +0100

    ✨ First commit (routing)

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$availableRoutes = ['home', ''];
+
+$route = 'home';
+if (isset($_GET['page']) and in_array($_GET['page'], $availableRoutes)) {
+    $route = $_GET['page'];
+}
+
+require './views/layout.phtml';
