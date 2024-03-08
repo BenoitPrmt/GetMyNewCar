2c3f67197df80885ef5eb88c75185d5bef203afc
BenoitPrmt <benoitparmentier45@gmail.com>
2024-03-08 11:49:01 +0100

    🩹 Fix userId bug

--- src/controllers/home_controller.php
+++ src/controllers/home_controller.php
@@ -1,24 +1,25 @@
 <?php
 
-$user = new User([
-    'id' => 1,
-    'username' => 'JohnDoe',
-    'email' => 'a@a.fr',
-    'password' => 'password'
-]);
+// TODO : remove that
+//$user = new User([
+//    'id' => 1,
+//    'username' => 'JohnDoe',
+//    'email' => 'a@a.fr',
+//    'password' => 'password'
+//]);
 //$userId = $userManager->createOne($user);
 
-$car = new Car([
-    'id' => 1,
-    'brand' => 'Renault',
-    'model' => 'Clio',
-    'price' => 10000,
-    'kilometers' => 20000,
-    'image' => 'https://www.laboutique-egarage.com/cdn/shop/files/CLIO2AVEXT.jpg?v=1690458450',
-    'year' => 2019,
-    'userId' => $userId,
-    'isSold' => false
-]);
+//$car = new Car([
+//    'id' => 1,
+//    'brand' => 'Renault',
+//    'model' => 'Clio',
+//    'price' => 10000,
+//    'kilometers' => 20000,
+//    'image' => 'https://www.laboutique-egarage.com/cdn/shop/files/CLIO2AVEXT.jpg?v=1690458450',
+//    'year' => 2019,
+//    'userId' => $userId,
+//    'isSold' => false
+//]);
 //$carManager->createOne($car);
 
 require './src/views/home.phtml';
