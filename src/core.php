42738b6ba219e027a20fdc6ea00c50c4c4d6b38b
BenoitPrmt <benoitparmentier45@gmail.com>
2024-03-08 10:00:34 +0100

    📥 Added database connection

---
+++ src/core.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+
+global $bdd;
+
+if (PHP_OS == 'WINNT') {
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=get_my_new_car;charset=utf8', 'root', '');
+        $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+} else if (PHP_OS == 'Darwin') {
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=get_my_new_car;charset=utf8', 'root', 'root');
+        $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+} else {
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=get_my_new_car;charset=utf8', 'root', 'root');
+        $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+
