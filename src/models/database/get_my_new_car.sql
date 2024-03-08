b63beebb8085ff38728b3d98ca23e0c3c13aff68
BenoitPrmt <benoitparmentier45@gmail.com>
2024-03-08 11:38:20 +0100

    🦾 Added database model

--- src/models/database/get_my_new_car.sql
+++ src/models/database/get_my_new_car.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:8889
--- Généré le : ven. 08 mars 2024 à 09:00
+-- Généré le : ven. 08 mars 2024 à 10:36
 -- Version du serveur : 5.7.39
--- Version de PHP : 7.4.33
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,8 +35,37 @@ CREATE TABLE `cars` (
   `kilometers` int(11) NOT NULL,
   `image` varchar(500) NOT NULL,
   `date` year(4) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `is_sold` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `cars`
+--
+
+INSERT INTO `cars` (`id`, `brand`, `model`, `price`, `kilometers`, `image`, `date`, `user_id`, `is_sold`) VALUES
+(3, 'Renault', 'Clio', 10000, 20000, 'https://www.laboutique-egarage.com/cdn/shop/files/CLIO2AVEXT.jpg?v=1690458450', 2019, 2, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'JohnDoe', 'a@a.fr', 'password'),
+(2, 'JohnDoe', 'a@a.fr', 'password');
 
 --
 -- Index pour les tables déchargées
@@ -46,6 +75,13 @@ CREATE TABLE `cars` (
 -- Index pour la table `cars`
 --
 ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `car_cars_user_id` (`user_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -56,7 +92,23 @@ ALTER TABLE `cars`
 -- AUTO_INCREMENT pour la table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `cars`
+--
+ALTER TABLE `cars`
+  ADD CONSTRAINT `car_cars_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
